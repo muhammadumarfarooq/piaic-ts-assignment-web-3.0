f5de355990da78154ea2ae0811dcd12c950469f2
Muhammad Umar Farooq <umar@craveup.com>
2023-07-22 17:29:11 +0500

    print person name

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
+// Exercise URL: https://github.com/panaverse/typescript-node-projects/blob/main/getting-started-exercises.md
+
+// 1
 /*
 * STEPS FOR RUNNING TS
 * npm install -g typescript ts-node
+* tsc --help => to see if TS compile is ready to compile
+*
 * */
+
+// 2. PRINT PERSON NAME
+
+const personName = "Hello Umar, would you like to learn TS?"
+console.log(personName);
